b2d26677449bfea14768662691b8a05b403689d3
mg <micah@cleardigital.com>
2023-06-26 21:37:24 -0400

    init

---
+++ footer.php
@@ -0,0 +1,13 @@
+</main><!-- main -->
+
+<footer>
+    <div class="footer container">
+
+        <p>Copyright &copy; <?php echo esc_html(date('Y')); ?> All Rights Reserved.</p>
+
+    </div>
+</footer>
+
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ functions.php
@@ -0,0 +1,6 @@
+<?php
+require_once('hamfunctions.php');
+hammer();
+
+// LEAVE THIS FILE EMPTY
+// ANY FUNCTIONS SHOULD GO IN /inc/Admin/Functions.php
